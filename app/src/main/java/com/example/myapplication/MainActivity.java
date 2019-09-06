5ff1bebcda97c822e0f1e5c29ec171d25d67b190
Rizwan Ramadhan <rizwanramadhan@student.telkomuniversity.ac.id>
2019-09-06 08:52:58 +0700

    add todo 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TODO 1 : Tambahan Activity
     }
 }
